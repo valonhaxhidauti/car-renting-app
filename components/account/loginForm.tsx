fd8c6b972ead98372c55996d61037af6a4be6be6
valon haxhidauti <valonidv10@hotmail.com>
2024-07-17 22:48:47 +0200

    Commented facebook and google login

--- components/account/loginForm.tsx
+++ components/account/loginForm.tsx
@@ -12,13 +12,13 @@ import { CheckIcon, FacebookIcon, GoogleIcon } from "@/assets/svgs";
 import { useTranslations } from "next-intl";
 import { LoginFormValues } from "@/lib/types";
 import { Loader2 } from "lucide-react";
-import { useAuth } from "../context/authContext"; 
+import { useAuth } from "../context/authContext";
 
 export default function LoginForm() {
   const t = useTranslations("Account");
   const locale = useTranslations()("Locale");
   const router = useRouter();
-  const { setAuthenticated, setToken } = useAuth(); 
+  const { setAuthenticated, setToken } = useAuth();
   const translations = {
     emailRequired: t("validation.emailRequired"),
     emailInvalid: t("validation.emailInvalid"),
@@ -84,7 +84,7 @@ export default function LoginForm() {
           if (data.data.attributes.email_verified_at === null) {
             setUnprocessedErrorMessage(translations.verifyEmail);
           } else {
-            setAuthenticated(true); 
+            setAuthenticated(true);
             setToken(token);
             router.push("/");
           }
@@ -104,14 +104,17 @@ export default function LoginForm() {
 
   return (
     <div className="flex mobile:bg-white w-full">
-      <div className="items-start flex flex-col flex-grow tablet:w-1/3 min-h-64 gap-6 justify-between mobile:p-16">
+      <div className="items-start flex flex-col flex-grow tablet:w-1/3 min-h-64 gap-6 mobile:p-16">
         <h1 className="text-grayFont font-bold text-4xl mb-2">
           {t("login.title")}
         </h1>
         <div className="w-20 h-0.5 bg-primary"></div>
         <p className="text-grayFont">{t("login.description")}</p>
-        <div className="flex flex-col gap-4 w-full">
-          <div className="flex flex-col tablet:flex-row gap-6 w-full px-8 tablet:px-0">
+        <div
+          className={`flex flex-col gap-2 w-full self-end justify-self-end h-full`}
+        >
+          {/* Facebook and Google Login */}
+          {/* <div className="flex flex-col tablet:flex-row gap-6 w-full px-8 tablet:px-0">
             <button className="flex gap-4 p-4 justify-center w-full bg-white shadow-btnShadow hover:bg-slate-50 active:bg-slate-100">
               <FacebookIcon />
               <p className="text-grayFont"> {t("login.facebookButton")}</p>
@@ -125,7 +128,7 @@ export default function LoginForm() {
             <div className=" border-b border-zinc-200 w-full"></div>
             <span className="px-4 text-slate-400 italic">{t("login.or")}</span>
             <div className=" border-b border-zinc-200 w-full"></div>
-          </div>
+          </div> */}
           <div
             className={`text-red-500 font-medium transition-opacity duration-300 ${
               unprocessedErrorMessage ? "opacity-100" : "opacity-0"
@@ -133,57 +136,32 @@ export default function LoginForm() {
           >
             {unprocessedErrorMessage}
           </div>
-          <div className="w-full">
-            <form className="flex flex-col gap-4" onSubmit={submitForm}>
-              <div>
-                <div className="mt-2 relative">
-                  <label className="block text-sm font-medium leading-6 text-grayFont">
-                    {t("login.emailLabel")}
-                  </label>
-                  <input
-                    type="email"
-                    value={formData.email}
-                    onChange={(e) => handleInputChange("email", e.target.value)}
-                    className={`block w-full rounded-sm text-grayFont p-4 focus-visible:outline-primary pr-8 ${
-                      errors.email
-                        ? "outline outline-2 outline-red-500"
-                        : "border-zinc-300 border"
-                    }`}
-                  />
-                  <CheckIcon
-                    className={`absolute transition-opacity right-4 bottom-[22px] ${
-                      isEmailValid(formData.email) ? "opacity-100" : "opacity-0"
-                    }`}
-                  />
-                </div>
-                <p
-                  className={`text-xs p-2 opacity-0 text-red-500 w-full transition-opacity
-                    ${errors.email && "opacity-100"}`}
-                >
-                  {errors.email}
-                </p>
-              </div>
-              <div>
+          <div className="w-full h-full">
+            <form
+              className="flex flex-col gap-4 justify-between h-full"
+              onSubmit={submitForm}
+            >
+              <div className="flex flex-col gap-4">
                 <div>
                   <div className="mt-2 relative">
                     <label className="block text-sm font-medium leading-6 text-grayFont">
-                      {t("login.passwordLabel")}
+                      {t("login.emailLabel")}
                     </label>
                     <input
-                      type="password"
-                      value={formData.password}
+                      type="email"
+                      value={formData.email}
                       onChange={(e) =>
-                        handleInputChange("password", e.target.value)
+                        handleInputChange("email", e.target.value)
                       }
-                      className={`block w-full rounded-sm p-4 text-grayFont focus-visible:outline-primary pr-8 ${
-                        errors.password
+                      className={`block w-full rounded-sm text-grayFont p-4 focus-visible:outline-primary pr-8 ${
+                        errors.email
                           ? "outline outline-2 outline-red-500"
                           : "border-zinc-300 border"
                       }`}
                     />
                     <CheckIcon
                       className={`absolute transition-opacity right-4 bottom-[22px] ${
-                        isPasswordValid(formData.password)
+                        isEmailValid(formData.email)
                           ? "opacity-100"
                           : "opacity-0"
                       }`}
@@ -191,10 +169,44 @@ export default function LoginForm() {
                   </div>
                   <p
                     className={`text-xs p-2 opacity-0 text-red-500 w-full transition-opacity
-                    ${errors.password && "opacity-100"}`}
+                    ${errors.email && "opacity-100"}`}
                   >
-                    {errors.password}
+                    {errors.email}
                   </p>
+                </div>
+                <div>
+                  <div>
+                    <div className="mt-2 relative">
+                      <label className="block text-sm font-medium leading-6 text-grayFont">
+                        {t("login.passwordLabel")}
+                      </label>
+                      <input
+                        type="password"
+                        value={formData.password}
+                        onChange={(e) =>
+                          handleInputChange("password", e.target.value)
+                        }
+                        className={`block w-full rounded-sm p-4 text-grayFont focus-visible:outline-primary pr-8 ${
+                          errors.password
+                            ? "outline outline-2 outline-red-500"
+                            : "border-zinc-300 border"
+                        }`}
+                      />
+                      <CheckIcon
+                        className={`absolute transition-opacity right-4 bottom-[22px] ${
+                          isPasswordValid(formData.password)
+                            ? "opacity-100"
+                            : "opacity-0"
+                        }`}
+                      />
+                    </div>
+                    <p
+                      className={`text-xs p-2 opacity-0 text-red-500 w-full transition-opacity
+                    ${errors.password && "opacity-100"}`}
+                    >
+                      {errors.password}
+                    </p>
+                  </div>
                 </div>
               </div>
               <div className="flex justify-between items-center">
