fc271505b0e03d4637bc479e3e121fd3479295a4
Yllndrit Beka <yllndrit@omure.com>
2025-05-15 14:13:18 +0200

    Add google analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { NextIntlClientProvider, useMessages } from "next-intl";
@@ -25,6 +26,8 @@ export const metadata: Metadata = {
   description: "Car renting app",
 };
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
+
 export default function RootLayout({ children, params: { locale } }: Props) {
 
   // Set locale statically
@@ -32,22 +35,36 @@ export default function RootLayout({ children, params: { locale } }: Props) {
 
   const messages = useMessages();
 
-  return (
-      <ViewTransitions>
-        <html lang={locale}>
-        <body className={font.className}>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <BookingProvider>
-            <AuthProvider>{children}</AuthProvider>
-          </BookingProvider>
-          <Toaster />
+    return (
+        <ViewTransitions>
+            <html lang={locale}>
+            <body className={font.className}>
+            <NextIntlClientProvider locale={locale} messages={messages}>
+                <BookingProvider>
+                    <AuthProvider>{children}</AuthProvider>
+                </BookingProvider>
+                <Toaster />
+                <CookieConsent />
+            </NextIntlClientProvider>
 
-          {/* Include your custom Cookie Consent component */}
-          <CookieConsent />
-
-        </NextIntlClientProvider>
-        </body>
-        </html>
-      </ViewTransitions>
-  );
+            {GA_MEASUREMENT_ID && (
+                <>
+                    <Script
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+                        strategy="afterInteractive"
+                    />
+                    <Script id="gtag-init" strategy="afterInteractive">
+                        {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_MEASUREMENT_ID}');
+                `}
+                    </Script>
+                </>
+            )}
+            </body>
+            </html>
+        </ViewTransitions>
+    );
 }
